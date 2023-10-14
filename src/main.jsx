a60b9baa1fd00fddd1a0f40431ac0d02204ee9db
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-10-14 12:42:45 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -21,12 +21,7 @@ const theme = createTheme({
     secondary: {
       main: "#00000",
     },
-        customColor1: {
-      main: "#123456",
-    },
-    customColor2: {
-      main: "#789abc",
-    },
+    
   },
 });
 
